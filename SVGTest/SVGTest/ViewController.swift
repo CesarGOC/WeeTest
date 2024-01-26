760f7ee82eca142b49e153c876b5a0cb2c4fcc2a
Cesar Alejandro Guadarrama Ortega <cesar@MacBook-Air-de-Cesar.local>
2024-01-25 18:23:44 -0600

    Add SVGTest

---
+++ SVGTest/SVGTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SVGTest
+//
+//  Created by Cesar Alejandro Guadarrama Ortega on 25/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var zoomScale: CGFloat = 1.0
+    var initialTransform: CGAffineTransform = .identity
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+    
+    
+
+
+}
